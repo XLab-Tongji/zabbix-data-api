6ad84998b58655628c8c66291790a743d05ba0ac
Rain <iheyu22@163.com>
2018-03-14 13:14:59 +0800

    init spring boot

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 # zabbix-data-api
-# zabbix-data-api
---
+++ src/main/java/rainlf/zabbix/ZabbixApplication.java
@@ -0,0 +1,12 @@
+package rainlf.zabbix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZabbixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZabbixApplication.class, args);
+	}
+}
